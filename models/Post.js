aaf87487d58868785a76c5bd272665de12600686
Noah Mejia <84366215+Noah0217@users.noreply.github.com>
2021-09-16 16:50:44 -0600

    id and title
    Initialized the Post model started on the id and title.

--- models/Post.js
+++ models/Post.js
@@ -1,2 +1,22 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
+
+//initializes Post model
+class Post extends Model {}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+
+        },
+
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+    }
+)
