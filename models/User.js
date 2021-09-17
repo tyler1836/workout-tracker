19f961282bf225460301e9ba48f7ee3fee183e9d
brandon-stewart-rgb <brandon@pixeltree.us>
2021-09-17 16:34:00 -0600

    Just a User

--- models/User.js
+++ models/User.js
@@ -2,13 +2,13 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
-class Users extends Model {
+class User extends Model {
 	checkPassword(loginPw) {
 		return bcrypt.compareSync(loginPw, this.password);
 	}
 }
 
-Users.init(
+User.init(
 	{
 		id: {
 			type: DataTypes.INTEGER,
@@ -54,8 +54,8 @@ Users.init(
 		timestamps: false,
 		freezeTableName: true,
 		underscored: true,
-		modelName: 'Users',
+		modelName: 'User',
 	}
-);
+); 
 
-module.exports = Users;
+module.exports = User;
