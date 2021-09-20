ad853efcc977c6304d5fb5a224b458869690b3cf
Noah <noahm0217@gmail.com>
2021-09-19 19:25:52 -0600

    javascript added

--- public/assets/javascript/login.js
+++ public/assets/javascript/login.js
@@ -1,1 +1,27 @@
-//
+async function loginFormHandler(event) {
+    event.preventDefault();
+  
+    const email = document.querySelector('#email-login').value.trim();
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    //route to login page
+    if (email && password && username) {
+      const response = await fetch('/api/users/login', {
+        method: 'post',
+        body: JSON.stringify({
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+
+  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
