9a734990c643a8193296e17cc1c596fa01f39fc0
Noah <noahm0217@gmail.com>
2021-09-21 17:16:43 -0600

    delete note function

--- public/javascript/delete-note.js
+++ public/javascript/delete-note.js
@@ -2,5 +2,27 @@ async function deleteFormHandler(event) {
 
     event.preventDefault();
 
-    const response = await fetch(``)
-}
+    //routes to notes
+    const response = await fetch(`/api/posts/${id}`, {
+        //deleting active note
+        method: 'DELETE',
+        body: JSON.stringify({
+        post_id: id
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      
+      if (response.ok) {
+
+        document.location.replace('/dashboard/');
+
+      } else {
+
+        alert(response.statusText);
+      }
+    
+  }
+  
+   document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
