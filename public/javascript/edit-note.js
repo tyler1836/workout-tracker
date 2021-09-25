eac495f7106f6be23ab9b945840756f78465a8b0
brandon-stewart-rgb <brandon@pixeltree.us>
2021-09-25 09:18:49 -0600

    edit note

--- public/javascript/edit-note.js
+++ public/javascript/edit-note.js
@@ -1,28 +1,28 @@
 async function editFormHandler(event) {
-    event.preventDefault();
-  
-    const title = document.querySelector('input[name="post-title"]').value;
-    const post_content = document.querySelector('input[name="post-content"]').value;
-    const id = window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
-      ];
+	event.preventDefault();
 
-    const response = await fetch(`/api/posts/${id}`, {
-        method: 'PUT',
-        body: JSON.stringify({
-            title,
-            post_content
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-      });
-      
-      if (response.ok) {
-        document.location.replace('/dashboard/');
-      } else {
-        alert(response.statusText);
-      }
-  }
-  
-  document.querySelector('.orange').addEventListener('submit', editFormHandler);
+	const title = document.querySelector('title').value;
+	const post_text = document.querySelector('comment_text').value;
+	const id = window.location.toString().split('/')[
+		window.location.toString().split('/').length - 1
+	];
+
+	const response = await fetch(`/api/posts/${id}`, {
+		method: 'PUT',
+		body: JSON.stringify({
+			title,
+			post_text,
+		}),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+
+	if (response.ok) {
+		document.location.replace('/dashboard/');
+	} else {
+		alert(response.statusText);
+	}
+}
+
+document.querySelector('.orange').addEventListener('submit', editFormHandler);
